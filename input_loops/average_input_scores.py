565750e205209bf8157dfff092af2bd9483b4643
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-20 11:54:52 -0500

    Added main in average_input_score.py. Still need to ensure two decimal place is used for average() result

--- input_loops/average_input_scores.py
+++ input_loops/average_input_scores.py
@@ -21,5 +21,28 @@ def average(score_list):
 
 
 if __name__ == "__main__":
-    pass
+    print("Welcome! Please complete the following as they appear below.\n")
+    first_name = input("First Name: ")
+    last_name = input("\nLast Name: ")
+    score_input_list = []
+    sentinel = -1
+    has_input = True
+
+
+    while (has_input):
+      user_input = float(input("\nEnter score for calculation, or -1 to stop: "))
+      # print(f'{user_input}' + " " + f'{sentinel}') // Used to verify what is being passed in testing
+      # print(score_input_list) // Used to verify what is being passed in testing
+      if user_input == sentinel:
+        has_input = False
+        break
+      else:
+        score_input_list.append(user_input)
+    
+    
+    average_score = average(score_input_list)
+
+
+    print(f'{last_name.capitalize()}' + ", " + f'{first_name.capitalize()}' + ": Average Score is " + f'{average_score}')
+
 
