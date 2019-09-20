10cd16112d3cd59e029c7c71f9e07122edadb007
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-20 07:53:12 -0500

    added functionality to average() and test_average() passes

--- input_loops/average_input_scores.py
+++ input_loops/average_input_scores.py
@@ -4,7 +4,20 @@ The purpose of this assignment is to demonstrate the use of while loops with use
 """
 
 def average(score_list):
-  pass
+  sum = 0
+  number_of_scores = len(score_list)
+
+
+  for score in score_list:
+    sum = sum + score
+  
+
+  avg_score = sum / number_of_scores
+
+
+  return avg_score
+
+
 
 
 if __name__ == "__main__":
